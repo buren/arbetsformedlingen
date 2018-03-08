9d792e2d22e875d0aaef5aed42c667e146093ee2
Jacob Burenstam <burenstam@gmail.com>
2018-03-08 21:39:52 +0100

    Refactor WSOccupationClient

--- lib/arbetsformedlingen/api/ws_occupation_client.rb
+++ lib/arbetsformedlingen/api/ws_occupation_client.rb
@@ -6,44 +6,44 @@ module Arbetsformedlingen
     class WSOccupationClient
       attr_reader :request
 
-      BASE_URL = 'https://api.arbetsformedlingen.se/af/v0/Occupation'.freeze
+      SERVICE_URL = 'https://api.arbetsformedlingen.se/af/v0/Occupation/wsoccupation.asmx'.freeze
 
       def initialize
-        @request = request || SOAPRequest.new(base_url: BASE_URL)
+        @request = request || SOAPRequest.new(SERVICE_URL)
       end
 
       def occupation(id)
-        soap_body = Arbetsformedlingen::SOAPBuilder.wrap do |body|
+        soap_body = SOAPBuilder.wrap do |body|
           body.GetOccupationById(xmlns: 'urn:ams.se:wsoccupation') do |node|
             node.occupationId(id)
           end
         end
 
-        request.post('/wsoccupation.asmx', soap_body.to_xml)
+        request.post(soap_body.to_xml)
       end
 
       def occupations
-        soap_body = Arbetsformedlingen::SOAPBuilder.wrap do |body|
+        soap_body = SOAPBuilder.wrap do |body|
           body.GetAllOccupations(xmlns: 'urn:ams.se:wsoccupation')
         end
 
-        request.post('/wsoccupation.asmx', soap_body.to_xml)
+        request.post(soap_body.to_xml)
       end
 
       def occupations_short
-        soap_body = Arbetsformedlingen::SOAPBuilder.wrap do |body|
+        soap_body = SOAPBuilder.wrap do |body|
           body.GetAllOccupationsShort(xmlns: 'urn:ams.se:wsoccupation')
         end
 
-        request.post('/wsoccupation.asmx', soap_body.to_xml)
+        request.post(soap_body.to_xml)
       end
 
       def occupations_detailed
-        soap_body = Arbetsformedlingen::SOAPBuilder.wrap do |body|
+        soap_body = SOAPBuilder.wrap do |body|
           body.GetAllOccupationsDetailed(xmlns: 'urn:ams.se:wsoccupation')
         end
 
-        request.post('/wsoccupation.asmx', soap_body.to_xml)
+        request.post(soap_body.to_xml)
       end
     end
   end
