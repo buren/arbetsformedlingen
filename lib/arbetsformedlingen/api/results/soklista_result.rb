4b9768356fcfef3fb17a057a20a8f5808674f01c
Jacob Burenstam <burenstam@gmail.com>
2018-09-03 21:02:23 +0200

    Extract method SoklistResult

--- lib/arbetsformedlingen/api/results/soklista_result.rb
+++ lib/arbetsformedlingen/api/results/soklista_result.rb
@@ -12,13 +12,17 @@ module Arbetsformedlingen
           total_vacancies: data.fetch('totalt_antal_ledigajobb', 0),
           raw_data: response_data,
           data: data.fetch('sokdata', []).map do |result|
-            Values::SoklistaResult.new(
-              id: result.fetch('id'),
-              name: result.fetch('namn'),
-              total_ads: result.fetch('antal_platsannonser'),
-              total_vacancies: result.fetch('antal_ledigajobb')
-            )
+            build_search_result(result)
           end
+        )
+      end
+
+      def self.build_search_result(result)
+        Values::SoklistaResult.new(
+          id: result.fetch('id'),
+          name: result.fetch('namn'),
+          total_ads: result.fetch('antal_platsannonser'),
+          total_vacancies: result.fetch('antal_ledigajobb')
         )
       end
     end
