ce401ff669bf8a97fb61bf793b459ef4c5dc8eba
Albert Arvidsson <albert.arvidsson@gmail.com>
2018-09-10 16:50:19 +0200

    Fix spec error on ruby 2.3.0
    Encoding::CompatibilityError
    incompatible encoding regexp match (UTF-8 regexp with ASCII-8BIT string)

--- spec/arbetsformedlingen/api/client_spec.rb
+++ spec/arbetsformedlingen/api/client_spec.rb
@@ -140,7 +140,7 @@ RSpec.describe Arbetsformedlingen::API::Client do
 
       expect { client.ads(county_id: 1, page: 9999, page_size: 1000) }
         .to raise_error(Arbetsformedlingen::API::Values::MatchningError,
-                        /Fel vid hämtning av annonslista/)
+                        /Fel vid h.+mtning av annonslista/)
     end
   end
 
