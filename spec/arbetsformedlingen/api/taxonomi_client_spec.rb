ebb01cafbd9216f309a03beed2872604885137a8
Jacob Burenstam <burenstam@gmail.com>
2018-12-08 22:02:19 +0100

    Refactor TaxonomiClient tests

--- spec/arbetsformedlingen/api/taxonomi_client_spec.rb
+++ spec/arbetsformedlingen/api/taxonomi_client_spec.rb
@@ -3,82 +3,60 @@
 require 'spec_helper'
 
 RSpec.describe Arbetsformedlingen::API::TaxonomiClient do
-  describe '#occupation_names' do
-    let(:sv_lang_id) { 502 }
+  [
+    {
+      method_name: :occupation_names,
+      args: { language_id: 502  },
+      css: 'OccupationName Term',
+      expected: { value: '1:e Fartygsingenjör/1:e Maskinist', length: 3262 }
+    },
+    {
+      method_name: :aid_occupation_names,
+      css: 'AIDOccupationName OccupationNameID',
+      expected: { value: '6818', length: 199 }
+    },
+    {
+      method_name: :continents,
+      args: { language_id: 502  },
+      css: 'Continent Term',
+      expected: { value: 'Antarktis', length: 22 }
+    },
+    {
+      method_name: :countries,
+      args: { language_id: 502  },
+      css: 'Country Term',
+      expected: { value: 'Afghanistan', length: 250 }
+    },
+    {
+      method_name: :drivers_licenses,
+      args: { language_id: 502  },
+      css: 'DrivingLicence Term',
+      expected: { value: 'A1', length: 15 }
+    },
+    {
+      method_name: :eu_regions,
+      args: { language_id: 502  },
+      css: 'EURegion Term',
+      expected: { value: 'A Coruña', length: 1362 }
+    },
+  ].each do |data|
+    method = data.fetch(:method_name)
+    expected = data.fetch(:expected)
 
-    it 'returns response that includes first occupation name', vcr: true do
-      client = described_class.new
-      response = client.occupation_names(language_id: sv_lang_id)
+    describe "##{method}" do
+      it "returns #{method}", vcr: true do
+        client = described_class.new
+        response = if data.key?(:args)
+                     client.public_send(method, **data.fetch(:args))
+                   else
+                     client.public_send(method)
+                   end
 
-      names = response.xml.css('OccupationName Term')
-      first_name = names.first.text
-      expect(first_name).to eq('1:e Fartygsingenjör/1:e Maskinist')
-      expect(names.length).to eq(3262)
-    end
-  end
-
-  describe '#aid_occupation_names' do
-    it 'returns response that includes first AID occupation id', vcr: true do
-      client = described_class.new
-      response = client.aid_occupation_names
-
-      ids = response.xml.css('AIDOccupationName OccupationNameID')
-      first_name = ids.first.text
-      expect(first_name).to eq('6818')
-      expect(ids.length).to eq(199)
-    end
-  end
-
-  describe '#continents' do
-    let(:sv_lang_id) { 502 }
-
-    it 'returns continents', vcr: true do
-      client = described_class.new
-      response = client.continents(language_id: sv_lang_id)
-
-      names = response.xml.css('Continent Term')
-      first_name = names.first.text
-      expect(first_name).to eq('Antarktis')
-      expect(names.length).to eq(22)
-    end
-  end
-
-  describe '#countries' do
-    let(:sv_lang_id) { 502 }
-
-    it 'returns countries', vcr: true do
-      client = described_class.new
-      response = client.countries(language_id: sv_lang_id)
-      names = response.xml.css('Country Term')
-      first_name = names.first.text
-      expect(first_name).to eq('Afghanistan')
-      expect(names.length).to eq(250)
-    end
-  end
-
-  describe '#drivers_licenses' do
-    let(:sv_lang_id) { 502 }
-
-    it 'returns drivers_licenses', vcr: true do
-      client = described_class.new
-      response = client.drivers_licenses(language_id: sv_lang_id)
-      names = response.xml.css('DrivingLicence Term')
-      first_name = names.first.text
-      expect(first_name).to eq('A1')
-      expect(names.length).to eq(15)
-    end
-  end
-
-  describe '#eu_regions' do
-    let(:sv_lang_id) { 502 }
-
-    it 'returns eu_regions', vcr: true do
-      client = described_class.new
-      response = client.eu_regions(language_id: sv_lang_id)
-      names = response.xml.css('EURegion Term')
-      first_name = names.first.text
-      expect(first_name).to eq('A Coruña')
-      expect(names.length).to eq(1362)
+        ids = response.xml.css(data.fetch(:css))
+        first_name = ids.first.text
+        expect(first_name).to eq(expected.fetch(:value))
+        expect(ids.length).to eq(expected.fetch(:length))
+      end
     end
   end
 end
