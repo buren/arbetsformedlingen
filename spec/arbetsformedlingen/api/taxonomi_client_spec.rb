54001788e8d75b5dc9af47ed9e16d0ebb7254d29
Jacob Burenstam <burenstam@gmail.com>
2018-12-08 22:18:30 +0100

    rubocop spec/ :lipstick:

--- spec/arbetsformedlingen/api/taxonomi_client_spec.rb
+++ spec/arbetsformedlingen/api/taxonomi_client_spec.rb
@@ -6,68 +6,68 @@ RSpec.describe Arbetsformedlingen::API::TaxonomiClient do
   [
     {
       method_name: :occupation_names,
-      args: { language_id: 502  },
+      args: { language_id: 502 },
       css: 'OccupationName Term',
-      expected: { value: '1:e Fartygsingenjör/1:e Maskinist', length: 3262 }
+      expected: { value: '1:e Fartygsingenjör/1:e Maskinist', length: 3262 },
     },
     {
       method_name: :aid_occupation_names,
       css: 'AIDOccupationName OccupationNameID',
-      expected: { value: '6818', length: 199 }
+      expected: { value: '6818', length: 199 },
     },
     {
       method_name: :continents,
-      args: { language_id: 502  },
+      args: { language_id: 502 },
       css: 'Continent Term',
-      expected: { value: 'Antarktis', length: 22 }
+      expected: { value: 'Antarktis', length: 22 },
     },
     {
       method_name: :countries,
-      args: { language_id: 502  },
+      args: { language_id: 502 },
       css: 'Country Term',
-      expected: { value: 'Afghanistan', length: 250 }
+      expected: { value: 'Afghanistan', length: 250 },
     },
     {
       method_name: :drivers_licenses,
-      args: { language_id: 502  },
+      args: { language_id: 502 },
       css: 'DrivingLicence Term',
-      expected: { value: 'A1', length: 15 }
+      expected: { value: 'A1', length: 15 },
     },
     {
       method_name: :eu_regions,
-      args: { language_id: 502  },
+      args: { language_id: 502 },
       css: 'EURegion Term',
-      expected: { value: 'A Coruña', length: 1362 }
+      expected: { value: 'A Coruña', length: 1362 },
     },
     {
       method_name: :employment_durations,
-      args: { language_id: 502  },
+      args: { language_id: 502 },
       css: 'EmploymentDuration Term',
-      expected: { value: 'Tillsvidare', length: 5 }
+      expected: { value: 'Tillsvidare', length: 5 },
     },
     {
       method_name: :employment_types,
-      args: { language_id: 502  },
+      args: { language_id: 502 },
       css: 'EmploymentType Term',
-      expected: { value: 'Vanlig anställning', length: 4 }
+      expected: { value: 'Vanlig anställning', length: 4 },
     },
     {
       method_name: :experiences_last,
-      args: { language_id: 502  },
+      args: { language_id: 502 },
       css: 'OccupationExperienceLast OccupationExperienceLastID',
-      expected: { value: '1', length: 28 }
+      expected: { value: '1', length: 28 },
     },
     {
       method_name: :experiences_year,
-      args: { language_id: 502  },
+      args: { language_id: 502 },
       css: 'OccupationExperienceYear OccupationExperienceYearID',
-      expected: { value: '1', length: 5 }
+      expected: { value: '1', length: 5 },
     },
     {
       method_name: :aido_occupation_names,
-      args: { label: '101010'  },
+      args: { label: '101010' },
       css: 'GetAIDOccupationNameByLabelResult OccupationNameID',
-      expected: { value: '6818', length: 1 }
+      expected: { value: '6818', length: 1 },
     },
   ].each do |data|
     method = data.fetch(:method_name)
