434a67a452645f199a25d20f38f9854b8284571f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-29 12:35:07 +0800

    Delete spec directory

--- spec/arbetsformedlingen_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-RSpec.describe Arbetsformedlingen do
-  it 'has a version number' do
-    expect(Arbetsformedlingen::VERSION).not_to be nil
-  end
-end
